5b343875030f8a608862d133a95c299d0ade66c3
Joao Marcos <joao_marcos50@outlook.com>
2026-07-02 13:51:00 -0300

    teste final 6

--- backend/.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Salva o cache na raiz do projeto, FORA da node_modules
-  cacheDirectory: join(__dirname, '.puppeteer_cache'),
-};
